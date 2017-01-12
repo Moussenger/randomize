a88e80adf43429fab3e0029c5b3cd0d77c6b40c5
Mou <dhmourelle@gmail.com>
2017-01-12 19:20:22 +0100

    Add random number generation to index page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule Randomize.PageController do
   use Randomize.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", number: :rand.uniform(100)
   end
 end
